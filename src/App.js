2d6d7ab9adbab97bf926286390009e7696adb494
Eyad911 <Eyad.Abdulhamid.elq.js@tuwaiq.edu.sa>
2021-12-25 13:44:03 +0300

    MVC FrontEnd

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+require("dotenv").config();
+
+const App = () => {
+  return (
+    <>
+      <Routes>
+       
+        
+      </Routes>
+    </>
+  );
+};
+
+export default App;
